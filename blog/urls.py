c551d9e59ad212760878382c48f852a532f89a86
Shashank Shreshtha <109666224+ShashankShreshtha@users.noreply.github.com>
2024-09-02 02:18:37 +0530

    Initial Commit

---
+++ blog/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from . import views
+from .views import HomePageView  # Import the view for the homepage
+
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.CreatePostView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+    path('post/<int:pk>/remove/', views.PostDeleteView.as_view(), name='post_remove'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('', HomePageView.as_view(), name='home'),
+
+]
